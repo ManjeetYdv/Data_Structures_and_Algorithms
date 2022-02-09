6ed86d0d32023572b99927724fc085f9946f9662
Manjeet Yadav <99329532+ManjeetYdv@users.noreply.github.com>
2022-02-09 17:27:01 +0530

    Create CountSort.cpp

---
+++ Arrays/Sorting algos/CountSort.cpp
@@ -0,0 +1,58 @@
+
+#include<iostream>
+#include<climits>
+using namespace std;
+
+
+void CountSort(int arr[] ,int n){
+	
+	int max =INT_MIN;
+	
+	for(int i=0 ; i<n ;i++){
+		if(arr[i]>max){
+			max = arr[i];
+		}	
+	}
+	
+	int count[max+1];
+	
+	for(int i=0 ;i<max+1 ;i++){
+		count[i]=0;
+	}
+	
+	for(int i=0 ;i<n ;i++){
+		count[arr[i]]++;
+		
+	}
+	
+	for(int i =1 ; i<max+1 ; i++){
+		count[i]=count[i]+ count[i-1];
+		
+	}
+	
+	int sorted[n];
+	
+	for(int i= n-1 ;i>=0 ;i--){
+    sorted[--count[arr[i]]]=arr[i];
+		
+	}
+	
+	for(int i= 0;i<n ;i++){
+		arr[i]=sorted[i];
+	}
+		
+}
+int main(){
+	
+	int arr[]={2,3,5,6,1};
+	int n= sizeof(arr)/sizeof(arr[1]);
+	
+	CountSort(arr , n);                     
+	
+	cout<<"Sorted array : ";
+	for(int i=0 ; i<n ;i++){
+		cout<<arr[i]<<" ";
+	}
+	cout<<endl;
+	
+}
