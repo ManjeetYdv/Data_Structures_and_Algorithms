6dac7af4afad3884ff9e714da9257f9d2fd5fb0e
Manjeet Yadav <99329532+ManjeetYdv@users.noreply.github.com>
2022-03-24 16:30:37 +0530

    Update rotate circular left n right array.cpp

--- Arrays/rotate circular left n right array.cpp
+++ Arrays/rotate circular left n right array.cpp
@@ -1,63 +1,45 @@
 #include<iostream>
 using namespace std;
 
-int* circularRight(int arr[] , int n){
+
+void rotateRight(int arr[] , int n){
+
+    int p = arr[n-1];
     
-    int* temp =new int[n];
-    int x= arr[n-1];
-    int z=1;
-    temp[0]=x;
-    for(int i =0; i<n-1 ; i++){
-    	temp[z++]=arr[i];
-    	
-	}	 	
-return temp;	
+    int j= arr[0];
+    
+    for(int i=1 ;i<n ;i++){
+        int temp= arr[i];
+        arr[i]=j;
+        j= temp;
+        
+        
+    }
+    arr[0]=p;
 }
 
+void rotateLeft(int arr[] , int n){
 
-int* circularLeft(int arr[] , int n)
-{
-    int* temp = new int[n];
-    temp[n-1]=arr[0];
-    int z =0;
-    
-    for(int i =1; i<n ; i++){
-    	temp[z++]=arr[i];
-	}
-    
-	return temp;
-	
+  int p= arr[0];
+  int j=arr[n-1];
+  
+  for(int i=n-2 ;i>=0 ;i--){
+      int temp = arr[i];
+      arr[i]=j;
+      j= temp;
+  }
+   arr[n-1]=p;
+
+
 }
 
+int main(){
 
-int main()
-{
-	int arr[]={2,3,4,5,7};
-	int n = 5;
+    int arr[]={1,2,3,4,5};
+    int n = sizeof(arr)/sizeof(arr[0]);
 
-   int *temp = circularRight(arr, 5);
-
- 
-	
-		for(int i=0 ; i<5; i++){
-		cout<<temp[i]<<" ";
-		
-	}
-	cout<<endl;
-	
-	
-	 int *temp2 = circularLeft(arr, 5);
-
-	for(int i=0 ; i<5; i++){
-		cout<<temp2[i]<<" ";
-		
-	}
-	cout<<endl;
-	
-	
-	
-	
+    rotateRight(arr , n);
+    for(int i=0 ;i<n ;i++){
+        cout<<arr[i]<<" ";
+    }
 }
-
-
-
