0b3f5354179da93581d7259dda1e70faed9fae01
Manjeet Yadav <99329532+ManjeetYdv@users.noreply.github.com>
2022-04-05 21:49:30 +0530

    Update maxProductSubarray.cpp

--- Arrays/maxProductSubarray.cpp
+++ Arrays/maxProductSubarray.cpp
@@ -1,76 +1,40 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
-int maxSubArr(int arr[] , int n){
-	
-	int max_ending=1;
-	int min_ending=1;
-	int highest =1;
-	
-	for(int i=0 ; i<n ;i++){
-		
-		if(arr[i]>0){
-			max_ending = max_ending*arr[i];
-			
-			min_ending= min(min_ending*arr[i] , 1);
-			
-			if(max_ending>highest){
-				highest = max_ending;
-				
-			}
-			
-			
-	}
-		if(arr[i]<0){
-			int temp = max_ending;
-			
-	    	 max_ending =max(min_ending*arr[i], 1 );
-		
-		     min_ending = temp*arr[i];
-		     
-		     if(max_ending>highest){
-				highest = max_ending;
-				
-			}
-		    	
-		}
-		 if(arr[i]==0){
-		 	
-         min_ending= 1;	 
-		 max_ending= 1;
-		 
-		 if(max_ending>highest){
-			highest = max_ending;
-				
-			}
-		 		 	
-		 	
-		 	
-		 }
-		
-	}
-	
-	return highest;
-	
+long long maxProductSubArr(vector<int> arr){
+
+     int n = arr.size();
+
+     long long curproduct=1,maxproduct=INT_MIN;
+        for(int i=0;i<n;i++){
+            
+            curproduct=curproduct*arr[i];
+            maxproduct=max(maxproduct,curproduct);
+            if(curproduct==0){
+                curproduct=1;
+            }
+            
+        }
+        curproduct=1;
+        for(int i=n-1;i>=0;i--){
+            
+            curproduct=curproduct*arr[i];
+            maxproduct=max(maxproduct,curproduct);
+            if(curproduct==0){
+                curproduct=1;
+            }
+            
+        }
+        return maxproduct;
+
 }
+
 int main(){
-	int arr[]={-9 , -9 , 10, -9 , -9 };
-	int n= sizeof(arr)/sizeof(arr[0]);
-	
-	int max = maxSubArr(arr , n );
-	
-	cout<<"Max Sub array product : "<<max;	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+    vector<int>arr = {2, 3, 4, 5, -1, 0};
+    int maxSub = maxProductSubArr(arr);
+
+    cout<<"highest product sub array : "<<maxSub;
+ 
+
+
 }
